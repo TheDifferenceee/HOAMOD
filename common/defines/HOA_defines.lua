d84028e664744a722d94a6fee7d17434c4c0b39c
TheDifferenceee <skelkicoglu1@gmail.com>
2023-08-14 09:35:21 +0300

    bitti error.

--- common/defines/HOA_defines.lua
+++ common/defines/HOA_defines.lua
@@ -170,8 +170,3 @@ NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 30                -- This much progre
 NDefines_Graphics.NGraphics.CAPITAL_ICON_CUTOFF = 800						-- 1100 
 NDefines_Graphics.NGraphics.DRAW_MAP_OBJECTS_CUTOFF = 250				-- 550 
 NDefines_Graphics.NGraphics.MAP_BUILDINGS_SHRINK_DISTANCE = 100				-- 180
-
-
-
-
-
