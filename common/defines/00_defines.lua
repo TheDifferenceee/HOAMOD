8edb6ab65e2b00cc16792fac96745e48c352e8f7
TheDifferenceee <skelkicoglu1@gmail.com>
2023-08-14 05:58:59 +0300

    düzeltme1

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -112,6 +112,18 @@ NNavy = {
     COMBAT_DAMAGE_TO_ORG_FACTOR = 0.5,                            -- casting damage value to ship organisation multiplier. Use it to balance the game difficulty. 
 	EXPERIENCE_FACTOR_CARRIER_GAIN = 0.02,                            -- Xp gain by carrier ships in the combat
 	NAVAL_INVASION_PREPARE_HOURS = 24,                                -- base hours needed to prepare an invasion
+    MISSION_FUEL_COSTS = {  -- fuel cost for each mission
+		0.0, -- HOLD (consumes fuel HOLD_MISSION_MOVEMENT_COST fuel while moving)
+		0.4, -- PATROL		
+		0.4, -- STRIKE FORCE (does not cost fuel at base, and uses IN_COMBAT_FUEL_COST in combat. this is just for the movement in between)	
+		0.4, -- CONVOY RAIDING
+		0.2, -- CONVOY ESCORT
+		1.0, -- MINES PLANTING	
+		1.0, -- MINES SWEEPING	
+		0.6, -- TRAIN
+		0.0, -- RESERVE_FLEET (consumes fuel HOLD_MISSION_MOVEMENT_COST fuel while moving)
+		1.0, -- NAVAL_INVASION_SUPPORT (does not cost fuel at base, only costs while doing bombardment and escorting units)
+	},
 	AGGRESSION_SETTINGS_VALUES = { -- ships will use this values while deciding to attack enemies
         0.1,        -- do not engage
         0.5,    -- low
