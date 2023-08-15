04c28ee32830924527e34113aa5cfbfe3aeaff9a
TheDifferenceee <skelkicoglu1@gmail.com>
2023-08-16 01:30:02 +0300

    hoa vanilla

--- common/defines/HOA_graphics.lua
+++
@@ -1,3 +0,0 @@
-NDefines_Graphics.NGraphics.CAPITAL_ICON_CUTOFF = 800						-- 1100 
-NDefines_Graphics.NGraphics.DRAW_MAP_OBJECTS_CUTOFF = 250				-- 550 
-NDefines_Graphics.NGraphics.MAP_BUILDINGS_SHRINK_DISTANCE = 100				-- 180
--- descriptor.mod
+++
@@ -1,7 +0,0 @@
-version="1.12.14"
-tags={
-	"Historical"
-}
-name="HOAMOD"
-supported_version="1.12.14"
-remote_file_id="3019073217"
