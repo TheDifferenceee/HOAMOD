a2ed79d3a8c2e61e6c0ea847728807704a8745fd
Magandalf <56162834+Magandalf@users.noreply.github.com>
2023-09-28 00:22:01 +0300

    Revert "Update HOA_defines.lua"
    This reverts commit 26c1da86cb4248bcff876ddbcaded6dc70b0c4cc.

--- common/defines/HOA_defines.lua
+++ common/defines/HOA_defines.lua
@@ -121,9 +121,9 @@ NDefines.NMilitary.ARMOR_VS_AVERAGE = 0.4                            -- how to w
 NDefines.NMilitary.PEN_VS_AVERAGE = 0.4 --0.4 yani vanilla deger                       
 NDefines.NMilitary.GARRISON_ORDER_ARMY_CAP_FACTOR = 1.0            -- armies gets increased cap when they are garrisoned --3.0 yani vanilla deger      
 NDefines.NMilitary.ARMY_TRAINING_FUEL_MULT = 1.0                    -- fuel consumption ratio while training --1.0 yani vanilla deger
-NDefines.NMilitary.COMBAT_STACKING_START = 2                       -- vanilla is 8 -- at what nr of divisions stacking penalty starts
+NDefines.NMilitary.COMBAT_STACKING_START = 3                       -- vanilla is 8 -- at what nr of divisions stacking penalty starts
 NDefines.NMilitary.COMBAT_STACKING_EXTRA = 1                      -- vanilla is 4 -- extra stacking from directions
-NDefines.NMilitary.COMBAT_STACKING_PENALTY = -0.05                -- vanilla is -0.02 -- how much stacking penalty per division      
+NDefines.NMilitary.COMBAT_STACKING_PENALTY = -1                -- vanilla is -0.02 -- how much stacking penalty per division      
 	
 NDefines.NAir.COMBAT_DAMAGE_SCALE = 0.5                            -- Higher value = more shot down planes --1 yani vanilla deger
 NDefines.NAir.COMBAT_DAMAGE_SCALE_CARRIER = 1                    -- same as above but used inside naval combat for carrier battles --5  f3ten koydugumuz ucakla ayni islevde oldugunu dusunuyoruz        
