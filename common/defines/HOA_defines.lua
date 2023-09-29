458509bdc28b5f8cbcbc9d0c107649d7a7caabef
TheDifferenceee <142064167+TheDifferenceee@users.noreply.github.com>
2023-09-29 14:18:46 +0300

    sup updatesss ve stacking silindi

--- common/defines/HOA_defines.lua
+++ common/defines/HOA_defines.lua
@@ -122,9 +122,6 @@ NDefines.NMilitary.ARMOR_VS_AVERAGE = 0.4                            -- how to w
 NDefines.NMilitary.PEN_VS_AVERAGE = 0.4 --0.4 yani vanilla deger                       
 NDefines.NMilitary.GARRISON_ORDER_ARMY_CAP_FACTOR = 1.0            -- armies gets increased cap when they are garrisoned --3.0 yani vanilla deger      
 NDefines.NMilitary.ARMY_TRAINING_FUEL_MULT = 1.0                    -- fuel consumption ratio while training --1.0 yani vanilla deger
-NDefines.NMilitary.COMBAT_STACKING_START = 3                       -- vanilla is 8 -- at what nr of divisions stacking penalty starts
-NDefines.NMilitary.COMBAT_STACKING_EXTRA = 1                      -- vanilla is 4 -- extra stacking from directions
-NDefines.NMilitary.COMBAT_STACKING_PENALTY = -1                -- vanilla is -0.02 -- how much stacking penalty per division      
 NDefines.NMilitary.PREFERRED_TACTIC_CHARACTER_SKILL_LEVEL_REQUIRED = 1
 	
 NDefines.NAir.COMBAT_DAMAGE_SCALE = 0.5                            -- Higher value = more shot down planes --1 yani vanilla deger
